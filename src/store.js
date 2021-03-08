8a1b8966c273995f8d04def6580f30dadf3ea501
Greg Johle <greg.johle@gmail.com>
2021-03-08 15:33:42 -0500

    assignment complete

---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+  {
+    title: "Google",
+    url: "www.google.com",
+    description: "The place to Google things",
+    rating: 5,
+    id: 1,
+  },
+];
+
+module.exports = { bookmarks };
